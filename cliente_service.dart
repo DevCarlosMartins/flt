56678b370e20976cecda1dd94c184454d01c8555
Seu Prefeito😜 <145706422+DevCarlosMartins@users.noreply.github.com>
2026-06-17 15:11:00 -0300

    Add files via upload

---
+++ cliente_service.dart
@@ -0,0 +1,11 @@
+class ClienteModel {
+  String? nome;
+  String? endereco;
+  String? urlImagem;
+
+  ClienteModel({
+    required this.nome,
+    required this.endereco,
+    required this.urlImagem,
+  });
+}
